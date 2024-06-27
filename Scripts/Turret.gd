70899f3caf2668a57eae73aa30d6fbf076aff8ba
pesesito <marcorguezhdez@gmail.com>
2024-06-27 12:13:05 +0200

    Modified turret

--- Scripts/Turret.gd
+++ Scripts/Turret.gd
@@ -1,9 +1,24 @@
 extends Node2D
 
 var bullet = load("res://Scenes/OtherStuff/bullet.tscn")
+var inZone = false
+
+onready var timer = $Timer
+
+func _process(delta):
+	if inZone and timer.is_stopped():
+		timer.start(1.5)
+		shoot()
 
 func _on_Detection_body_entered(body):
 	if body.is_in_group("player"):
-		var b = bullet.instance()
-		add_child(b)
-		b.global_position = self.position
+		inZone = true
+
+func _on_Detection_body_exited(body):
+	if inZone and body.is_in_group("player"):
+		inZone = false
+
+func shoot():
+	var b = bullet.instance()
+	add_child(b)
+	b.global_position = self.position
--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,12 +1,24 @@
 extends Node2D
 
-var speed = 25
+var speed = 30
+var stopLook : bool = false
 
 func _process(delta):
-	look_at(GLOBALS.player_pos)
+	if not stopLook:
+		look_at(GLOBALS.player_pos)
 	
 	position += transform.x * speed * delta
+	speed += 0.01
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("player"):
 		Input.action_press("dead")
+		queue_free()
+
+
+func _on_Timer_timeout():
+	stopLook = true
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
