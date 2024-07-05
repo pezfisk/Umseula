9534fd99e7de9e06b1505036d8f003b47bde3d16
pesesito <marcorguezhdez@gmail.com>
2024-07-05 18:27:32 +0200

    Android Export and bullet / turret fix

--- Scripts/Turret.gd
+++ Scripts/Turret.gd
@@ -5,14 +5,10 @@ var inZone = false
 
 onready var timer = $Timer
 
-func _process(delta):
-	if inZone and timer.is_stopped():
-		timer.start(1.5)
-		shoot()
-
 func _on_Detection_body_entered(body):
 	if body.is_in_group("player"):
 		inZone = true
+		shoot()
 
 func _on_Detection_body_exited(body):
 	if inZone and body.is_in_group("player"):
@@ -22,3 +18,10 @@ func shoot():
 	var b = bullet.instance()
 	add_child(b)
 	b.global_position = self.position
+	timer.start(2)
+
+func _on_Timer_timeout():
+	print("Stopped!")
+	if inZone:
+		shoot()
+
--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,24 +1,31 @@
 extends Node2D
 
 var speed = 30
-var stopLook : bool = false
+var stopLook : bool = true
+var startAnim : bool = true
 
 func _process(delta):
 	if not stopLook:
 		look_at(GLOBALS.player_pos)
-	
-	position += transform.x * speed * delta
-	speed += 0.01
+		
+	if startAnim:
+		position.y -= delta * 20
+		self.global_rotation_degrees = -90
+	else:
+		position += transform.x * speed * delta
+		speed += 0.01
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("player"):
 		Input.action_press("dead")
 		queue_free()
 
-
-func _on_Timer_timeout():
-	stopLook = true
-
-
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+func _on_rotTimer_timeout():
+	stopLook = true
+
+func _on_startTimer_timeout():
+	stopLook = false
+	startAnim = false
