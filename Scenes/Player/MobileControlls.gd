e2dcc7682bf1a0e451acdbec69409d5f63f5d08f
pesesito <marcorguezhdez@gmail.com>
2024-07-31 15:48:37 +0200

    Added FPS counter

--- Scenes/Player/MobileControlls.gd
+++ Scenes/Player/MobileControlls.gd
@@ -19,3 +19,7 @@ func _on_Up_pressed():
 	Input.action_press("ui_up")
 func _on_Up_released():
 	Input.action_release("ui_up")
+
+# warning-ignore:unused_argument
+func _physics_process(delta):
+	$Label.text = "FPS: " + str(Engine.get_frames_per_second()) + " MSPF: "+ str(1/Engine.get_frames_per_second())
