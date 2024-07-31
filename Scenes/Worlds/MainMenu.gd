d6f4b92e9eab68ed191bbd3f31a9e00e0c9d4863
pesesito <marcorguezhdez@gmail.com>
2024-07-31 15:33:32 +0200

    Fixed Start button doesnt work when Config.cfg doesnt exist

--- Scenes/Worlds/MainMenu.gd
+++ Scenes/Worlds/MainMenu.gd
@@ -3,19 +3,20 @@ extends Node2D
 func _on_Start_pressed():
 # warning-ignore:return_value_discarded
 
-	if DataParser.getData() != null:
+	if DataParser.getData().size() != 0:
 
 		var dataStr = DataParser.loadData("WorldSave", "currentWorld")
 
 		var loadedData = dataStr.split("-", true, 2)
-		
+
 		var loadedWorld = loadedData[0].split("_", true, 2)[1]
 		var loadedLevel = loadedData[1].split("_", true, 2)[1]
 
 		get_tree().change_scene("res://Scenes/Worlds/World" + str(loadedWorld) + "/Level" + str(loadedLevel) + ".tscn")
-		
+
 	else:
-		
+
+# warning-ignore:return_value_discarded
 		get_tree().change_scene("res://Scenes/Worlds/World1/Level1.tscn")
 
 
